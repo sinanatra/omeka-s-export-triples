bdf71e18e839772848edad287f08443f87146434
Giacomo Nanni <gn.nanni@gmail.com>
2021-03-03 12:51:45 +0100

    fix: added argparser to convert

--- convert.py
+++ convert.py
@@ -1,11 +1,26 @@
 from rdflib import Graph, plugin
 from rdflib.serializer import Serializer
-from utils.setup import *
+import argparse
 import urllib.request
+from utils.setup import *
 
-resource_id = 15
-resource_url = endpoint + str("/" + str(resource_id))
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--id', help='Resource Id', required=False)
+parser.add_argument('-t', '--type', help='Convert resource to a specific format',choices=['xml', 'n3', 'turtle'], required=True)
+args = parser.parse_args()
+
+if (args.id):
+    resource_url = endpoint + str("/" + str(args.id))
+else:
+    resource_url = endpoint + str("/")
+
 page = urllib.request.urlopen(resource_url)
 resource_content = page.read().decode('utf-8')
 g = Graph().parse(data=resource_content, format='json-ld')
-print(g.serialize(format='xml', indent=4).decode('utf-8'))
+
+if (args.type == 'xml'):
+    print(g.serialize(format='xml', indent=4).decode('utf-8'))
+if (args.type == 'n3'):
+    print(g.serialize(format='n3', indent=4).decode('utf-8'))
+if (args.type == 'turtle'):
+    print(g.serialize(format='turtle', indent=4).decode('utf-8'))
