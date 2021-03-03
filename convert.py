5a37b5e22a687ce57831943365fd01bc5431284f
Giacomo Nanni <gn.nanni@gmail.com>
2021-03-03 13:04:25 +0100

    save files to output

--- convert.py
+++ convert.py
@@ -9,18 +9,27 @@ parser.add_argument('-i','--id', help='Resource Id', required=False)
 parser.add_argument('-t', '--type', help='Convert resource to a specific format',choices=['xml', 'n3', 'turtle'], required=True)
 args = parser.parse_args()
 
+output_file = ''
+output_fileName = ''
+
 if (args.id):
     resource_url = endpoint + str("/" + str(args.id))
+    output_fileName = "item_" + str(args.id)
 else:
     resource_url = endpoint + str("/")
+    output_fileName = 'all'
 
 page = urllib.request.urlopen(resource_url)
 resource_content = page.read().decode('utf-8')
 g = Graph().parse(data=resource_content, format='json-ld')
 
 if (args.type == 'xml'):
-    print(g.serialize(format='xml', indent=4).decode('utf-8'))
+    output_file = g.serialize(format='xml', indent=4).decode('utf-8')
 if (args.type == 'n3'):
-    print(g.serialize(format='n3', indent=4).decode('utf-8'))
+    output_file = g.serialize(format='n3', indent=4).decode('utf-8')
 if (args.type == 'turtle'):
-    print(g.serialize(format='turtle', indent=4).decode('utf-8'))
+    output_file = g.serialize(format='turtle', indent=4).decode('utf-8')
+
+f = open("output/" + output_fileName + "." + args.type, "w")
+f.write(output_file)
+f.close()
