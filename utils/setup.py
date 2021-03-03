93cc31ff3d63b3167634739f7404f326dc22102d
Giacomo Nanni <gn.nanni@gmail.com>
2021-03-03 12:36:38 +0100

    fix: restructured folders

--- utils/setup.py
+++ utils/setup.py
@@ -1,11 +1,13 @@
 import configparser
 
+# Get configuration file
 config = configparser.ConfigParser()
 config.sections()
 config.read('./config/api.ini')
-
 client_id = config.get('main', 'client_id')
 client_secret = config.get('main', 'client_secret')
 endpoint = config.get('main', 'endpoint')
 
-print(client_secret)
+# Connect to Api
+access_url = endpoint+ '?key_identity=' + client_id + '&key_credential=' + client_secret
+print("Connected to:    ", access_url)
